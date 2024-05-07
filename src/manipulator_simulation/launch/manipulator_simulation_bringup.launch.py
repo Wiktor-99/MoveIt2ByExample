59b74864af79b1465ecb1bc1520c2f81b7e4023b
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-05-07 19:57:57 +0000

    Add /conveyor/cmd_vel topic to ros_gz_bridge

--- src/manipulator_simulation/launch/manipulator_simulation_bringup.launch.py
+++ src/manipulator_simulation/launch/manipulator_simulation_bringup.launch.py
@@ -59,6 +59,7 @@ def generate_launch_description():
         name="gz_bridge",
         arguments=[
             "/clock" + "@rosgraph_msgs/msg/Clock" + "[ignition.msgs.Clock",
+            "/conveyor/cmd_vel" + "@std_msgs/msg/Float64" + "]gz.msgs.Double",
         ],
         output="screen",
     )
